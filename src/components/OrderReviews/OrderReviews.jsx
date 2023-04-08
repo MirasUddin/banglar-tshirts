727117dc79a23c88041d57fe1427d307ab7e6661
MirasUddin <mirasuddin978@gmail.com>
2023-04-08 11:18:28 +0600

    fist commit

---
+++ src/components/OrderReviews/OrderReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReviews = () => {
+    return (
+        <div>
+            <h3>Orders Reviews</h3>
+        </div>
+    );
+};
+
+export default OrderReviews;
